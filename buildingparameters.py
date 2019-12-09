7c3d538f02bc1c03b65881dfd6532d8d30cc2ad0
mechurk <londynska56>
2019-12-09 22:55:05 +0100

    Add1

---
+++ buildingparameters.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import math
+from shapely.geometry import Polygon
+from lxml import etree
+import markup3dmodule
+
+#TODO osetrit fce
+#TODO citovat pouzite casti skriptu
+# --------------------------------------------------------------#
+# funcions
