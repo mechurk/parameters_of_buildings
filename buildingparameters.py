e2b109bbfe686be3281b46f9c63527efbee6f4f1
mechurk <londynska56>
2019-12-09 22:55:59 +0100

    Add2

--- buildingparameters.py
+++ buildingparameters.py
@@ -5,7 +5,6 @@ from shapely.geometry import Polygon
 from lxml import etree
 import markup3dmodule
 
-#TODO osetrit fce
-#TODO citovat pouzite casti skriptu
+
 # --------------------------------------------------------------#
 # funcions
